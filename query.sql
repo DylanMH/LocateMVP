022cef6dfbb4e2bb74c4df82899922ce380a27f8
Dylan <dylan98909@yahoo.com>
2026-08-19 19:35:02 -0500

    chore: remove temp test files

--- query.sql
+++
@@ -1,6 +0,0 @@
--- Check territories schema
-PRAGMA table_info(territories);
-
--- Check techs in Charles Clark's supervisor territory
-SELECT t.id, t.name, t.type FROM territories t WHERE t.id = 'terr-sup-ctx';
-SELECT * FROM territories WHERE type = 'TECH_TERRITORY' LIMIT 5;
